5fc6333e97281e1e2a9de3ff5e82bd7d540899f4
Abdul Moiz <moiz05366@gmail.com>
2025-08-21 15:30:02 +0500

    solve issue.

--- src/components/MainCard/MainCard.tsx
+++ src/components/MainCard/MainCard.tsx
@@ -1,16 +1,19 @@
 import ShareLinkGenerator from "../LinkGeneratorBox";
+import { useLocation } from "react-router-dom";
 
 interface MainCardProp {
     children: React.ReactNode;
 }
 
 const MainCard = ({ children }: MainCardProp) => {
+    const location = useLocation();
+    const showShare = location.pathname === "/" || location.pathname.startsWith("/view/");
     return (
         <>
             <div className="bg-blue-200 min-w-full min-h-[460px] mt-8 shadow-2xl flex">
                 {children}
             </div>
-            <ShareLinkGenerator />
+            {showShare && <ShareLinkGenerator />}
         </>
     )
 }
