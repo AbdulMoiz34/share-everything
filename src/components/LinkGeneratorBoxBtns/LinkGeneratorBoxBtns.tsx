27f72ae38fee6bf2b0409f15eb7a56ce1ce8b2b4
Abdul Moiz <moiz05366@gmail.com>
2025-08-21 16:29:52 +0500

    Update LinkGeneratorBoxBtns.tsx

--- src/components/LinkGeneratorBoxBtns/LinkGeneratorBoxBtns.tsx
+++ src/components/LinkGeneratorBoxBtns/LinkGeneratorBoxBtns.tsx
@@ -3,6 +3,9 @@ import { DeleteBtn, LinkGeneratorButton } from "../../components/";
 import toast from "react-hot-toast";
 import { db, ref, remove } from "../../firebase";
 import { useNavigate } from "react-router-dom";
+import { useDeleteResourceMutation } from "../../store/services/filesApi";
+import { setFiles } from "../../store/reducers/fileReducer";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 
 interface LinkGeneratorBoxBtnsProps {
     id: string;
@@ -11,6 +14,9 @@ interface LinkGeneratorBoxBtnsProps {
 
 const LinkGeneratorBoxBtns = ({ id, setId }: LinkGeneratorBoxBtnsProps) => {
     const navigate = useNavigate();
+    const [deleteResource] = useDeleteResourceMutation();
+    const dispatch = useAppDispatch();
+    const files = useAppSelector((s) => s.fileUpload.files);
 
     const generateId = () => {
         const id = nanoid(6);
@@ -19,13 +25,23 @@ const LinkGeneratorBoxBtns = ({ id, setId }: LinkGeneratorBoxBtnsProps) => {
     };
 
     const deleteHandler = async () => {
-        localStorage.removeItem("id");
-        setId("");
-        navigate("");
+        toast.loading("Deleting..");
+
+        const promises = files.map(file => {
+            return deleteResource({ public_id: file.public_id, resource_type: file.resource_type }).unwrap();
+        })
+
         try {
+            await Promise.all(promises);
             await remove(ref(db, `shares/${id}`));
+            localStorage.removeItem("id");
+            setId("");
+            navigate("");
+            toast.dismiss();
             toast.success("Done.");
+            dispatch(setFiles([]));
         } catch (err) {
+            toast.dismiss();
             toast.error("something went wrong.");
         }
     }
@@ -44,7 +60,7 @@ const LinkGeneratorBoxBtns = ({ id, setId }: LinkGeneratorBoxBtnsProps) => {
     return (
         <div className="flex gap-4">
             <LinkGeneratorButton text="Generate" onClick={generateId} disabled={!!id.length} />
-            <DeleteBtn id={id} deleteHandler={deleteHandler}/>
+            <DeleteBtn id={id} deleteHandler={deleteHandler} />
             <LinkGeneratorButton text="Save" onClick={saveHandler} disabled={!id.length} />
             <LinkGeneratorButton text="Copy" onClick={copyUrlHandler} disabled={!id.length} />
         </ div>
